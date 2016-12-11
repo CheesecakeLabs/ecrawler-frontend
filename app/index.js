3b3e9a850ba7559b276dcdb720345b47941b3977
Guilherme Hayashi <guilherme@ckl.io>
2016-12-11 16:21:37 -0200

    Change PORT to 80.

--- app/index.js
+++ app/index.js
@@ -7,6 +7,6 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(process.env.PORT || 80, function () {
+  console.log('Example app listening on port 80!');
 });
