40ced23996c578515f3f5e94546cc61773d65b2b
Douglas Klein <douglas@ckl.io>
2017-04-10 09:00:23 -0300

    Setup store configurations to comply with boilerplate

--- src/store/configure-store.js
+++ src/store/configure-store.js
@@ -1,4 +1,4 @@
-/* eslint-disable global-require */
+/* eslint global-require: 0 */
 if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'test') {
   module.exports = require('./configure-store.prod')
 } else {
